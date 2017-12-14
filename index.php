632962963fb71881ea9f35371e352cc028b4fe69
Marie-Axelle Simoes <Vous@exemple.com>
2017-12-14 11:02:08 +0100

    ex 3

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Php fonctions</title>
+    </head>
+    <body>
+     
+        <?php
+        function melting($string1, $string2){
+            $result = $string1. ' '. $string2;
+            return $result;
+        }
+       echo melting('hello','wolrd');
+        ?>
+    </body>
+</html>
